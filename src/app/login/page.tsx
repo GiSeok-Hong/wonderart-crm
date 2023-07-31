c99d606db95559fad6289296907707a741a6e635
fepanbr <fepanbr@gmail.com>
2023-07-31 23:07:25 +0900

    feat: 로그인 UI 개발

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="h-full flex justify-center items-center bg-[#f3f3f3]">
+    <div className="h-full flex justify-center items-center bg-slate-50">
       <article className="flex gap-20 flex-col items-center w-96 h-176 bg-white shadow-lg">
         <Image
           width={242}
@@ -12,11 +12,28 @@ export default function LoginPage() {
           className="mt-32"
         />
         <div className="w-80 h-[343] flex-col">
-          <label htmlFor="email">Email</label>
-          <input name="email" className="w-full" type="email" />
-          <label htmlFor="password">password</label>
-          <input name="password" className="w-full" type="password" />
-          <button className="bg-[#91E9F2] w-full text-white py-3 px-10">
+          <div className="mb-5">
+            <label className="font-normal" htmlFor="email">
+              Email
+            </label>
+            <input
+              name="email"
+              className="w-full h-8 pl-2 shadow-inner"
+              type="email"
+            />
+          </div>
+          <div className="mb-5">
+            <label className="font-normal" htmlFor="password">
+              password
+            </label>
+            <input
+              name="password"
+              className="w-full h-8 pl-2 shadow-inner"
+              type="password"
+            />
+          </div>
+
+          <button className="bg-[#91E9F2] w-full text-white py-3 px-10 mb-6">
             Login
           </button>
           <p>Forgot your password?</p>
