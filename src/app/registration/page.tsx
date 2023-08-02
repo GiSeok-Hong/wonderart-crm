f9f4f2b88fd9cfc0ee56ede2acb0176ee5a8aec2
Giseok Hong <hgsok3056@gmail.com>
2023-08-02 19:21:08 +0900

    feat: 페이지별 title 설정, Header 적용

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "입학원서",
+  description: "원더아트 스튜디오 입학원서 페이지입니다.",
+};
+
 export default function RegistrationPage() {
   return <h1>입학원서 페이지</h1>;
 }
