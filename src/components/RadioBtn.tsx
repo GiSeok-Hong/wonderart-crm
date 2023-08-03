a18bbf14d608a66273347786131757b0d16ab70d
Giseok Hong <hgsok3056@gmail.com>
2023-08-04 01:50:54 +0900

    feat: 라디오버튼 컴포넌트 생성

---
+++ src/components/RadioBtn.tsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+
+export default function RadioBtn({
+  children,
+  value,
+  name,
+  disabled,
+  onChange,
+}: any) {
+  return (
+    <label className="text-xl ml-1 mr-5">
+      <input
+        type="radio"
+        value={value}
+        id={value}
+        name={name}
+        disabled={disabled}
+        onChange={onChange}
+        className="mr-2"
+      />
+      {children}
+    </label>
+  );
+}
