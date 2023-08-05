1ea6a501e3d8431009596f5a50cbd5d5eb4faa34
GiSeok Hong <hgsok3056@gmail.com>
2023-08-05 13:13:42 +0900

    #15 입학원서 페이지 퍼블리싱 (#19)
    * feat: 로고 이미지, 로고 텍스트 png 파일 추가
    
    * feat: favicon 이미지 추가
    
    * design: primary-color 추가
    
    * feat: Header 작성
    
    * feat: 페이지별 title 설정, Header 적용
    
    * feat: minWidth 추가
    
    입학원서 페이지의 label 사이즈를 맞추기 위해서 추가함
    
    * feat: 라디오버튼 컴포넌트 생성
    
    * feat: 입학원서 페이지 퍼블리싱 작성
    
    * refactor: 입학원서 페이지의 form 부분을 컴포넌트로 분리

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "학원생",
+  description: "원더아트 스튜디오 학원생 리스트 페이지입니다.",
+};
+
 export default function ListPage() {
   return <h1>학원생 리스트 페이지</h1>;
 }
--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "시간표",
+  description: "원더아트 스튜디오 시간표 페이지입니다.",
+};
+
 export default function SchedulePage() {
   return <h1>시간표 페이지</h1>;
 }
