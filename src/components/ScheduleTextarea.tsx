6405f395d5076d0d1ef4f05a5b39a524a67aba41
Giseok Hong <hgsok3056@gmail.com>
2023-08-13 15:00:49 +0900

    feat: ScheduleTextarea 컴포넌트 작성

---
+++ src/components/ScheduleTextarea.tsx
@@ -0,0 +1,15 @@
+const TEXTAREA_CLASS =
+  "bg-gray-EEE p-2 outline-primary-color resize-none w-44 h-[200px] block border-[1px] border-white";
+
+export default function ScheduleTextarea() {
+  return (
+    <textarea
+      name=""
+      id=""
+      cols={30}
+      rows={7}
+      className={TEXTAREA_CLASS + " "}
+      placeholder="노트"
+    ></textarea>
+  );
+}
