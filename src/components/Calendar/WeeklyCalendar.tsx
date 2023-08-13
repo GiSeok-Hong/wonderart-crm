b1d9ebc6dbb25bd1b24303eb1c7090b34319562c
Giseok Hong <hgsok3056@gmail.com>
2023-08-13 15:24:41 +0900

    feat: WeeklyCalendar 작성

--- src/components/Calendar/WeeklyCalendar.tsx
+++ src/components/Calendar/WeeklyCalendar.tsx
@@ -1,99 +1,25 @@
 "use client";
 
-import {
-  Calendar,
-  TimeGrid,
-  momentLocalizer,
-  Views,
-  Navigate,
-} from "react-big-calendar";
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
 import "moment/locale/ko";
-// import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import WeeklyCalendarToolbar from "./WeeklyCalendarToolbar";
-import { useMemo } from "react";
-import PropTypes from "prop-types";
-import * as dates from "date-arithmetic";
-
-function MyWeek({
-  date,
-  localizer,
-  max = localizer.endOf(new Date(), "day"),
-  min = localizer.startOf(new Date(), "day"),
-  scrollToTime = localizer.startOf(new Date(), "day"),
-  ...props
-}: any) {
-  const currRange = useMemo(
-    () => MyWeek.range(date, { localizer }),
-    [date, localizer]
-  );
-
-  return (
-    <TimeGrid
-      date={date}
-      eventOffset={15}
-      localizer={localizer}
-      max={max}
-      min={min}
-      range={currRange}
-      scrollToTime={scrollToTime}
-      {...props}
-    />
-  );
-}
-
-MyWeek.propTypes = {
-  date: PropTypes.instanceOf(Date).isRequired,
-  localizer: PropTypes.object,
-  max: PropTypes.instanceOf(Date),
-  min: PropTypes.instanceOf(Date),
-  scrollToTime: PropTypes.instanceOf(Date),
-};
-
-MyWeek.range = (date: any, { localizer }: any) => {
-  const start = date;
-  const end = dates.add(start, 2, "day");
-
-  let current = start;
-  const range = [];
-
-  while (localizer.lte(current, end, "day")) {
-    range.push(current);
-    current = localizer.add(current, 1, "day");
-  }
-
-  return range;
-};
-
-MyWeek.navigate = (date: any, action: any, { localizer }: any) => {
-  switch (action) {
-    case Navigate.PREVIOUS:
-      return localizer.add(date, -3, "day");
-
-    case Navigate.NEXT:
-      return localizer.add(date, 3, "day");
-
-    default:
-      return date;
-  }
-};
-
-MyWeek.title = (date: { toLocaleDateString: () => any }) => {
-  return `My awesome week: ${date.toLocaleDateString()}`;
-};
 
 export default function WeeklyCalendar() {
   moment.locale("ko");
   const localizer = momentLocalizer(moment);
+  const year = new Date().getFullYear();
 
   return (
     <Calendar
       localizer={localizer}
-      style={{ height: 800 }}
+      style={{ minWidth: 600 }}
       components={{ toolbar: WeeklyCalendarToolbar }}
-      defaultView={Views.WEEK}
-      // defaultView={Views.WORK_WEEK}
+      views={["work_week"]}
+      defaultView={Views.WORK_WEEK}
+      min={new Date(year, 0, 1, 14, 0, 0)}
+      max={new Date(year, 0, 1, 19, 59, 59)}
     />
   );
 }
--- src/components/Calendar/WeeklyCalendarToolbar.tsx
+++ src/components/Calendar/WeeklyCalendarToolbar.tsx
@@ -12,22 +12,6 @@ export default function WeeklyCalendarToolbar(props: {
     onNavigate(action);
   };
 
-  // const now = date; // Mon Aug 07 2023 18:24:15 GMT+0900 (한국 표준시) {}
-  // const nowDayOfWeek = now.getDay(); // 1 월요일
-  // const nowDay = now.getDate(); // 7 일
-  // const nowMonth = now.getMonth() + 1; //8 월
-  // const nowYear = now.getFullYear(); // 2023
-  // const startDate = new Date(
-  //   nowYear,
-  //   nowMonth,
-  //   nowDay - nowDayOfWeek
-  // ).getDate();
-  // const endDate = new Date(
-  //   nowYear,
-  //   nowMonth,
-  //   nowDay + (6 - nowDayOfWeek)
-  // ).getDate();
-
   return (
     <div className="rbc-toolbar">
       <span className="rbc-btn-group">
