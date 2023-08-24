c42e532117ce0dfbe21ff727d18c3bc18e16735e
fepanbr@gmail.com <fepanbr@gmail.com>
2023-08-24 12:11:58 +0900

    feat: ID 오름차순으로 조회하도록 변경

--- src/service/student.ts
+++ src/service/student.ts
@@ -1,14 +1,4 @@
-import { Student } from '@/types/student';
-import { StudentList } from '../../data/studentMockData';
 import { prisma } from '../../lib/prisma';
-
-export async function getStudentMockData(studentId: string): Promise<Student> {
-  const studentData = await StudentList.find((student) => student.id.toString() === studentId);
-
-  if (!studentData) throw new Error(`학생 정보를 찾을 수 없습니다.`);
-
-  return studentData;
-}
 
 export async function getStudentList() {
   const studentList = await prisma.student.findMany({
@@ -17,7 +7,10 @@ export async function getStudentList() {
         select: {
           phone: true,
         }
-      }
+      },
+    },
+    orderBy: {
+      id: 'asc',
     }
   });
 
