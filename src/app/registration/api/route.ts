ad100db51c448d16a06d3111d3e19c7a4d500a5a
Giseok Hong <hgsok3056@gmail.com>
2023-08-30 00:42:16 +0900

    refactor: 비구조화 할당으로 수정

--- src/app/registration/api/route.ts
+++ src/app/registration/api/route.ts
@@ -2,28 +2,46 @@ import { NextResponse } from 'next/server';
 import { prisma } from '../../../../lib/prisma';
 
 export async function POST(req: Request) {
-  const studentData = await req.json();
+  const {
+    entranceDate,
+    day,
+    time,
+    name,
+    phone,
+    birthDate,
+    sex,
+    address,
+    school,
+    experience,
+    reason,
+    importantActivity,
+    interestingActivity,
+    caution,
+    agree,
+    guardianName,
+    guardianPhone,
+  } = await req.json();
 
   try {
     await prisma.student.create({
       data: {
-        entranceDate: new Date(studentData.entranceDate),
-        day: studentData.day,
-        time: studentData.time,
-        name: studentData.name,
-        phone: studentData.phone,
-        birthDate: new Date(studentData.birthDate),
-        sex: studentData.sex,
-        address: studentData.address,
-        school: studentData.school,
-        experience: studentData.experience,
-        reason: studentData.reason,
-        importantActivity: studentData.importantActivity,
-        interestingActivity: studentData.interestingActivity,
-        caution: studentData.caution,
-        agree: studentData.agree,
+        entranceDate: new Date(entranceDate),
+        day,
+        time,
+        name,
+        phone,
+        birthDate: new Date(birthDate),
+        sex,
+        address,
+        school,
+        experience,
+        reason,
+        importantActivity,
+        interestingActivity,
+        caution,
+        agree,
         guardian: {
-          create: { name: studentData.guardianName, phone: studentData.guardianPhone },
+          create: { name: guardianName, phone: guardianPhone },
         },
         // TODO: 학생 등록 할 때 알맞은 수업 생성 되도록
         class: {
