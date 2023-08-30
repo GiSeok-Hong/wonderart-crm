3b057ae8fb79b703345678afeac4102ab5c14419
CheolHyeon Park <fepanbr@gmail.com>
2023-08-30 23:27:17 +0900

    fix: birthDate 구하는 로직 변경 (#44)

--- src/helper/age.ts
+++ src/helper/age.ts
@@ -1,15 +1,15 @@
 /**
  * 만 나이를 구하는 함수
- * @param birthDate YYYY.MM.DD
+ * @param birthDate Date
  */
-export function getAge(birth: string) {
-  if (!birth) return 0;
-  const birthYear = Number(birth.substring(0, 4));
-  const birthMonth = Number(birth.substring(5, 7));
-  const birthDay = Number(birth.substring(8, 10));
-  const birthDate = new Date(birthYear, birthMonth, birthDay);
+export function getAge(birthDate: Date) {
   const today = new Date();
-  const age = yearDiff(today, birthDate);
+  const birth = new Date(birthDate);
+  let age = today.getFullYear() - birth.getFullYear();
+  const month = today.getMonth() - birth.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < birth.getDate())) {
+    age--;
+  }
   return age;
 }
 
