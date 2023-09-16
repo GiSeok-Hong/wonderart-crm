da1445d74e28b4e13411ba945a3368c68d36cba3
Giseok Hong <hgsok3056@gmail.com>
2023-09-17 01:39:29 +0900

    fix: .env 파일 변수명에 NEXT_PUBLIC_ 추가

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -9,7 +9,7 @@ const DEFAULT_SIGN_OPTION: SignOption = {
 };
 
 export function signJwtAccessToken(payload: JwtPayload, options: SignOption = DEFAULT_SIGN_OPTION) {
-  const secret_key = process.env.SECRET_KEY;
+  const secret_key = process.env.NEXT_PUBLIC_SECRET_KEY;
   const token = jwt.sign(payload, secret_key!, options);
 
   return token;
@@ -17,7 +17,7 @@ export function signJwtAccessToken(payload: JwtPayload, options: SignOption = DE
 
 export function verifyJwt(token: string) {
   try {
-    const secret_key = process.env.SECRET_KEY;
+    const secret_key = process.env.NEXT_PUBLIC_SECRET_KEY;
     const decoded = jwt.verify(token, secret_key!);
 
     return decoded as JwtPayload;
