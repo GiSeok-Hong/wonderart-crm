3e83f63edab1507eca1d688b3a97d2e8efb1bbf7
fepanbr <fepanbr@gmail.com>
2023-09-16 22:52:11 +0900

    feat: api 라우팅 변경

---
+++ src/app/api/student/[id]/route.ts
@@ -0,0 +1,67 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../../lib/prisma";
+
+type Params = {
+  id: string;
+};
+
+export async function PUT(request: NextRequest, context: { params: Params }) {
+  const { id } = context.params;
+  const body = await request.json();
+  if (body === undefined) {
+    return NextResponse.json({ error: 'Bad Request' }, { status: 400 })
+  }
+
+  const {
+    address,
+    birthDate,
+    entranceDate,
+    guardianName,
+    guardianPhone,
+    name,
+    phone,
+    sex,
+    school,
+    time,
+    day,
+    experience,
+    reason,
+    importantActivity,
+    interestingActivity,
+    teacherMemo,
+    caution,
+  } = body;
+
+  try {
+    const result = await prisma.student.update({
+      where: { id: Number(id) },
+      data: {
+        address,
+        birthDate,
+        entranceDate,
+        guardian: {
+          update: {
+            name: guardianName,
+            phone: guardianPhone,
+          }
+        },
+        name,
+        phone,
+        sex,
+        school,
+        time,
+        day,
+        experience,
+        reason,
+        importantActivity,
+        interestingActivity,
+        teacherMemo,
+        caution,
+      }
+    })
+    return NextResponse.json({ data: result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: '학생 정보 수정에 실패했습니다.' }, { status: 500 });
+  }
+
+}
