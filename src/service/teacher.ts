d36e5d849f39673c83e2a489a3c47551db327ab6
fepanbr@gmail.com <fepanbr@gmail.com>
2023-09-26 19:11:46 +0900

    feat: 이메일 중복 처리

--- src/service/teacher.ts
+++ src/service/teacher.ts
@@ -12,6 +12,14 @@ export async function createTeacher(data: { name: string, email: string, phone: 
 
   const { email, name, phone } = data;
 
+  const isExist = await prisma.teacher.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (isExist) throw new Error("이미 존재하는 이메일입니다.");
+
   try {
     const teacher = await prisma.teacher.create({
       data: {
