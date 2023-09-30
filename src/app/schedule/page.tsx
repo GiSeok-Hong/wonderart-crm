d420d8cf95972449e8acb087c4151446b18d790d
GiSeok Hong <hgsok3056@gmail.com>
2023-09-30 14:32:25 +0900

    Wonderart-CRM #62 시간표 페이지 mock data 대신 현재 학생 데이터가 나오도록 수정 (#65)
    * design: 시간표 페이지 헤더 캘린더를 푸터 캘린더로 변경
    
    * design: calendar css 수정
    
    * feat: 시간표 - 학생 데이터가 나오도록 변경
    
    mock data가 아닌 현재 입력된 학생 데이터를 보여주기만 함.
    
    * chore: 주석 제거

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,6 +1,7 @@
-import HeaderCalendar from '@/components/Calendar/HeaderCalendar';
+import FooterCalendar from '@/components/Calendar/FooterCalendar';
 import WeeklyCalendar from '@/components/Calendar/WeeklyCalendar';
 import ScheduleTextarea from '@/components/ScheduleTextarea';
+import { getStudentList } from '@/service/student';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -8,29 +9,31 @@ export const metadata: Metadata = {
   description: '원더아트 스튜디오 시간표 페이지입니다.',
 };
 
-export default function SchedulePage() {
+export default async function SchedulePage() {
+  const students = await getStudentList();
+
   return (
     <div>
       <div className="flex justify-between gap-2 mb-5">
-        <HeaderCalendar />
-        <textarea
-          name=""
-          id=""
-          cols={30}
-          rows={8}
-          className="bg-gray-EEE p-2 outline-primary-color resize-none w-2/3 h-[200px]"
-          placeholder="노트"
-        />
-      </div>
-      <div className="flex justify-between gap-2">
-        <WeeklyCalendar />
-        <div className="mt-[38px] pt-[67px]">
+        <WeeklyCalendar studentsData={students} />
+        <div className="mt-[38px] pt-[21px]">
           <ScheduleTextarea />
           <ScheduleTextarea />
           <ScheduleTextarea />
           <ScheduleTextarea />
           <ScheduleTextarea />
         </div>
+      </div>
+      <div className="flex justify-between gap-2">
+        <FooterCalendar />
+        <textarea
+          name=""
+          id=""
+          cols={30}
+          rows={8}
+          className="bg-gray-EEE p-2 outline-primary-color resize-none w-2/3 h-[200px] mb-10"
+          placeholder="노트"
+        />
       </div>
     </div>
   );
