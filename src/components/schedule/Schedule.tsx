af1cca33023af19ef07e803d48d7a1ac891047a0
Giseok Hong <hgsok3056@gmail.com>
2023-10-16 00:48:16 +0900

    feat: 전체 시간표 데이터 호출 후 요일별로 보이도록 기능 구현

--- src/components/schedule/Schedule.tsx
+++ src/components/schedule/Schedule.tsx
@@ -1,7 +1,51 @@
+'use client';
+
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import TableTimeColumn from './TableTimeColumn';
+import { useEffect, useState } from 'react';
+import { type Class, type Student } from '@prisma/client';
+import moment from 'moment';
+import { getAge } from '@/helper/age';
 
-export default function schedule() {
+type StudentItem = { student: Student } & { isAttendance: boolean };
+type ScheduleItem = Class & { studentList: StudentItem[] };
+
+const dayList = ['월', '화', '수', '목', '금'];
+
+export default function Schedule() {
+  const [allScheduleList, setAllScheduleList] = useState<ScheduleItem[]>([]);
+  const [weeklyScheduleList, setWeeklyScheduleList] = useState<ScheduleItem[]>([]);
+  const [startDate, setStartDate] = useState(() => {
+    const now = moment();
+    if (now.day() === 0) {
+      return now.add(1, 'days').format('YYYY-MM-DD');
+    } else if (now.day() === 6) {
+      return now.add(2, 'days').format('YYYY-MM-DD');
+    } else {
+      return now.subtract(now.day() - 1, 'days').format('YYYY-MM-DD');
+    }
+  });
+
+  const fetchAllScheduleList = async () => {
+    const scheduleList: ScheduleItem[] = await fetch(`/api/schedule`).then((res) => res.json());
+    setAllScheduleList(scheduleList);
+    calculateWeeklyScheduleList(scheduleList);
+  };
+
+  const calculateWeeklyScheduleList = (scheduleList: ScheduleItem[]) => {
+    const endDate = moment(startDate).add(4, 'days').format('YYYY-MM-DD');
+
+    const result = scheduleList?.filter((schedule) =>
+      moment(moment(schedule.classDate).format('YYYY-MM-DD')).isBetween(startDate, endDate, undefined, '[]'),
+    );
+
+    setWeeklyScheduleList(result);
+  };
+
+  useEffect(() => {
+    fetchAllScheduleList();
+  }, []);
+
   return (
     <div className="w-[600px] box-border">
       {/* 스케쥴 툴바 */}
@@ -9,7 +53,7 @@ export default function schedule() {
         <div>
           <AiOutlineArrowLeft />
         </div>
-        <h1 className="text-center">10월</h1>
+        <h1 className="text-center">{moment(startDate).format('MM월')}</h1>
         <div>
           <AiOutlineArrowRight />
         </div>
@@ -19,235 +63,137 @@ export default function schedule() {
       <div className="schedule-table">
         {/* 테이블 헤더 */}
         <div className="schedule-table-header  flex border-t border-black text-center h-[30px]">
-          <div className="w-[80px]  border-black border-x">빈 칸</div>
-          <div className="w-[104px]  border-black bg-primary-color border-r">9 월</div>
-          <div className="w-[104px]  border-black bg-primary-color border-r">10 화</div>
-          <div className="w-[104px]  border-black bg-primary-color border-r">11 수</div>
-          <div className="w-[104px]  border-black bg-primary-color border-r">12 목</div>
-          <div className="w-[104px]  border-black bg-primary-color border-r">13 금</div>
+          <div className="w-[80px]  border-black border-x"></div>
+          {dayList.map((day, i) => {
+            return (
+              <div
+                key={i}
+                className="w-[104px]  border-black bg-primary-color border-r"
+              >
+                {moment(startDate)?.add(i, 'days').format(`${day} (D일)`)}
+              </div>
+            );
+          })}
         </div>
 
         {/* 테이블 컨텐트 */}
         <div className="schedule-table-content flex ">
-          {/* table-time-column */}
           <TableTimeColumn />
-          {/* 월 */}
-          <div className="schedule-table-student-list-mon w-[104px] box-boder border-r border-t  border-black">
-            <div className="time-14 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
 
-            <div className="time-15 h-[180px] border-black border-b bg-gray-100">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
+          {weeklyScheduleList?.map((schedule, i) => {
+            return (
+              <div
+                key={schedule?.id}
+                className="schedule-table-student-list w-[104px] box-boder border-r border-t  border-black text-sm"
+              >
+                {/* 2시 수업 */}
+                <div className="h-[180px] border-black border-b">
+                  {schedule?.studentList
+                    .filter((studentItem) => {
+                      const { student } = studentItem;
+                      const classIndex = student?.day?.findIndex((day) => day === i + 1);
+                      return student?.time[classIndex] === 14;
+                    })
+                    .map((studentItem) => {
+                      const { student, isAttendance } = studentItem;
+                      // TODO: isAttendance input checkbox 형식으로 작업 필요
+                      return (
+                        <div
+                          key={student?.id}
+                          className="h-[30px]"
+                        >
+                          {student?.name}, {getAge(student?.birthDate)}세
+                        </div>
+                      );
+                    })}
+                </div>
 
-            <div className="time-16 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
+                {/* 3시 수업 */}
+                <div className="h-[180px] border-black border-b bg-gray-100">
+                  {schedule?.studentList
+                    .filter((studentItem) => {
+                      const { student } = studentItem;
+                      const classIndex = student?.day?.findIndex((day) => day === i + 1);
+                      return student?.time[classIndex] === 15;
+                    })
+                    .map((studentItem) => {
+                      const { student } = studentItem;
+                      return (
+                        <div
+                          key={student?.id}
+                          className="h-[30px]"
+                        >
+                          {student?.name}, {getAge(student?.birthDate)}세
+                        </div>
+                      );
+                    })}
+                </div>
 
-            <div className="time-17 h-[180px] border-black border-b bg-gray-100">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
+                {/* 4시 수업 */}
+                <div className="h-[180px] border-black border-b">
+                  {schedule?.studentList
+                    .filter((studentItem) => {
+                      const { student } = studentItem;
+                      const classIndex = student?.day?.findIndex((day) => day === i + 1);
+                      return student?.time[classIndex] === 16;
+                    })
+                    .map((studentItem) => {
+                      const { student } = studentItem;
+                      return (
+                        <div
+                          key={student?.id}
+                          className="h-[30px]"
+                        >
+                          {student?.name}, {getAge(student?.birthDate)}세
+                        </div>
+                      );
+                    })}
+                </div>
 
-            <div className="time-18 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-          </div>
-          {/* 화 */}
-          <div className="studmentlist-mon w-[104px] box-boder border-r border-t  border-black">
-            <div className="time-14 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
+                {/* 5시 수업 */}
+                <div className="h-[180px] border-black border-b bg-gray-100">
+                  {schedule?.studentList
+                    .filter((studentItem) => {
+                      const { student } = studentItem;
+                      const classIndex = student?.day?.findIndex((day) => day === i + 1);
+                      return student?.time[classIndex] === 17;
+                    })
+                    .map((studentItem) => {
+                      const { student } = studentItem;
+                      return (
+                        <div
+                          key={student?.id}
+                          className="h-[30px]"
+                        >
+                          {student?.name}, {getAge(student?.birthDate)}세
+                        </div>
+                      );
+                    })}
+                </div>
 
-            <div className="time-15 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-16 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-17 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-          </div>
-          {/* 수 */}
-          <div className="studmentlist-mon w-[104px] box-boder border-r border-t  border-black">
-            <div className="time-14 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-15 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-16 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-17 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-18 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-          </div>
-          {/* 목 */}
-          <div className="studmentlist-mon w-[104px] box-boder border-r border-t  border-black">
-            <div className="time-14 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-15 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-16 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-17 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-          </div>
-          {/* 금 */}
-          <div className="studmentlist-mon w-[104px] box-boder border-r border-t  border-black">
-            <div className="time-14 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-15 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-16 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-17 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-
-            <div className="time-18 h-[180px] border-black border-b">
-              <div className="h-[30px]">학생일, 7세</div>
-              <div className="h-[30px]">학생이, 7세</div>
-              <div className="h-[30px]">학생삼, 7세</div>
-              <div className="h-[30px]">학생사, 7세</div>
-              <div className="h-[30px]">학생오, 7세</div>
-              <div className="h-[30px]">학생육, 7세</div>
-            </div>
-          </div>
+                {/* 6시 수업 */}
+                <div className="h-[180px] border-black border-b">
+                  {schedule?.studentList
+                    .filter((studentItem) => {
+                      const { student } = studentItem;
+                      const classIndex = student?.day?.findIndex((day) => day === i + 1);
+                      return student?.time[classIndex] === 18;
+                    })
+                    .map((studentItem) => {
+                      const { student } = studentItem;
+                      return (
+                        <div
+                          key={student?.id}
+                          className="h-[30px]"
+                        >
+                          {student?.name}, {getAge(student?.birthDate)}세
+                        </div>
+                      );
+                    })}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
