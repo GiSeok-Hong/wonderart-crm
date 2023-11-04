b1bff396f9092af2a1b1ccbeda90a8b34fcb8b0f
Giseok Hong <hgsok3056@gmail.com>
2023-11-04 14:14:26 +0900

    style: 주석 제거 + h-full 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,30 +110,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           rel="shortcut icon"
           href="/favicon.ico"
         />
-
-        {/* <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
-        <meta name="twitter:title" content="PWA App" />
-        <meta name="twitter:description" content="Best PWA App in the world" />
-        <meta
-          name="twitter:image"
-          content="https://yourdomain.com/icons/android-chrome-192x192.png"
-        />
-        <meta name="twitter:creator" content="@DavidWShadow" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="PWA App" />
-        <meta property="og:description" content="Best PWA App in the world" />
-        <meta property="og:site_name" content="PWA App" />
-        <meta property="og:url" content="https://yourdomain.com" />
-        <meta
-          property="og:image"
-          content="https://yourdomain.com/icons/apple-touch-icon.png"
-        /> */}
       </head>
       <body className={inter.className}>
         <Providers>
           <Header />
-          <main className="h-full px-10 py-5">{children}</main>
+          <main className="px-10 py-5">{children}</main>
         </Providers>
         <div id="modal-root"></div>
         <Toaster />
