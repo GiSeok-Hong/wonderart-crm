c92917b35ea2db4d158992b0f632f19ae1d446b2
Giseok Hong <hgsok3056@gmail.com>
2023-11-16 13:27:54 +0900

    fix: phone 함수 내 조건 삭제

--- src/helper/phone.ts
+++ src/helper/phone.ts
@@ -1,7 +1,3 @@
 export function phoneFormat(phone: string) {
-  if (!phone) {
-    return '';
-  } else {
-    return phone.slice(0, 3) + '-' + phone.slice(3, 7) + '-' + phone.slice(7);
-  }
+  return phone.slice(0, 3) + '-' + phone.slice(3, 7) + '-' + phone.slice(7);
 }
