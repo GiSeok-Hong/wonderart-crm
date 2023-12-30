02a5b7766dc3dc9038c0888b1c7e5f1e4995dc7f
GiSeok Hong <hgsok3056@gmail.com>
2023-12-30 18:36:32 +0900

    Wonderart-CRM #86 Prisma schema 선생님 연락처 unique 설정 (#97)
    * feat: prisma 스키마 선생님 연락처 unique 설정 및 에러 처리
    
    * chore: 불필요한 import 제거

--- src/service/teacher.ts
+++ src/service/teacher.ts
@@ -12,13 +12,21 @@ export async function createTeacher(data: { name: string; email: string; phone: 
 
   const { email, name, phone, password } = data;
 
-  const isExist = await prisma.teacher.findUnique({
+  const isExistEmail = await prisma.teacher.findUnique({
     where: {
       email,
     },
   });
 
-  if (isExist) throw new Error('이미 존재하는 이메일입니다.');
+  if (isExistEmail) throw new Error('이미 존재하는 이메일입니다.');
+
+  const isExistPhone = await prisma.teacher.findUnique({
+    where: {
+      phone,
+    },
+  });
+
+  if (isExistPhone) throw new Error('이미 존재하는 연락처입니다.');
 
   try {
     const teacher = await prisma.teacher.create({
