4b0eebb5aa88986e2767471a0fd0b716c746c4f2
Giseok Hong <hgsok3056@gmail.com>
2024-04-25 18:47:52 +0900

    fix: cronjob 실행 메소드 post에서 get으로 변경

--- src/app/api/class/route.ts
+++ src/app/api/class/route.ts
@@ -2,7 +2,7 @@ import { createMonthlyClassList } from '@/service/class';
 import moment from 'moment';
 import { NextResponse } from 'next/server';
 
-export async function POST(request: Request) {
+export async function GET(request: Request) {
   // TODO: cron jobs 가 정상적으로 작동시 아래 코드 적용하여 확인해보기
   // const authHeader = request.headers.get('authorization');
   // if (authHeader !== `Bearer ${process.env.NEXT_PUBLIC_CRON_SECRET}`) {
